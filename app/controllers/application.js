b9fdcd20f7534d511fed211af09796a758c310ea
Drew Pappas <drewpappas@mac.com>
2016-03-27 11:35:45 -0700

    start fetching user data from /me endpoint

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  sessionUser: Ember.inject.service('session-user'),
   actions: {
     logout() {
       this.get('session').invalidate();
