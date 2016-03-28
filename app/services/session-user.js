edbea4084b58f10492b2bebd491a7cc01c45cf42
Drew Pappas <drewpappas@mac.com>
2016-03-27 17:07:58 -0700

    refactored authentication path

--- app/services/session-user.js
+++ app/services/session-user.js
@@ -5,7 +5,6 @@ const { service } = Ember.inject;
 export default Ember.Service.extend({
   session: service('session'),
   store: service(),
-
   user: Ember.computed('session.data.authenticated.access_token', function() {
     return this.get('store').findRecord('user', 'me');
   })
