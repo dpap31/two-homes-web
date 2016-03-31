beedc9ca6923d7a545d7ca2f797e6d417bba430c
Drew Pappas <drewpappas@mac.com>
2016-03-30 21:26:37 -0700

    added logout to navbar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
-  sessionUser: Ember.inject.service('session-user'),
-  actions: {
-    logout() {
-      this.get('session').invalidate();
-    }
-  }
 });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('register-user');
   this.route('login');
-  this.route('logout');
   this.route('conversations', function() {
     this.route('new');
   });
