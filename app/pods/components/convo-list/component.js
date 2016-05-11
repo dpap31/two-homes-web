502b1f9d85a48d56e7d33e3dfab472240e3e3d50
Drew Pappas <drewpappas@mac.com>
2016-05-10 18:08:48 -0700

    improved chat sidebar interface.

--- app/pods/components/convo-list/component.js
+++ app/pods/components/convo-list/component.js
@@ -3,6 +3,4 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   sessionUser: Ember.inject.service('session-user'),
-  // sortProps: ['name'],
-  // sortedConversations: Ember.computed.sort('parentingGroup.conversations', 'sortProps'),
 });
