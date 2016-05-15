cfe3325032574a54429dffbb5064796c50582c49
Drew Pappas <drewpappas@mac.com>
2016-05-15 13:01:33 -0700

    updated parening group invite to user ember paper

--- app/pods/parenting-group/new-invite/controller.js
+++ app/pods/parenting-group/new-invite/controller.js
@@ -6,8 +6,9 @@ export default Ember.Controller.extend({
   isDisabled: Ember.computed.not('isValid'),
 
   actions:{
-    sendInvite(invite){
+    sendInvite(){
       let controller = this;
+      let invite = controller.get('model')
       invite.set('email', controller.get('emailAddress'));
       invite.save().then(function(){
         controller.set('responseMessage', `Invite sent to: ${controller.get('emailAddress')}`);
