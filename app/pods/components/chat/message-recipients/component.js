e2360330e98f498a407736310332ac59f1d05ad0
Drew Pappas <drewpappas@mac.com>
2016-05-25 21:55:35 -0700

    added session service to participantcomponent

--- app/pods/components/chat/message-recipients/component.js
+++ app/pods/components/chat/message-recipients/component.js
@@ -1,13 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sessionUser: Ember.inject.service('sessionUser'),
 
 });
-
-function recipientTitleString(users){
-  if (users.get('length') > 1){
-    users.map(function(u){return u.get('firstName')}).join(", ").toString()
-  }else{
-    users.get('firstObject.fullName');
-  }
-}
