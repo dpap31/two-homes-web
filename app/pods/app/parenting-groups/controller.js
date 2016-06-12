965dbccca77a45c0fd41c42277a6060f2b914f70
Drew Pappas <drewpappas@mac.com>
2016-06-12 10:45:55 -0700

    added comments, change method naming conventions

--- app/pods/app/parenting-groups/controller.js
+++ app/pods/app/parenting-groups/controller.js
@@ -4,21 +4,31 @@ export default Ember.Controller.extend({
   sessionUser: Ember.inject.service('session-user'),
 
   actions: {
-    filterById(param) {
+    filterByUser(param) {
       if (param !== '') {
+        // Search Regex
         const regexString = '(' + param.split(' ').join(')+.*(') + ')+.*';
         const regex = new RegExp(regexString, 'ig');
-        let allUsers = this.get('store').peekAll('user')
-        let filteredUsers = allUsers.filter((item) => item.get('fullName').match(regex));
-        let filteredUsersMemberships = filteredUsers.getEach('memberships');
-        return Ember.RSVP.all(filteredUsersMemberships).then(function(membershipCollections){
-          var memberships = membershipCollections.reduce(function(sum, val){
-              return sum.pushObjects(val.toArray());
+
+        // Fetch all users in the local store, filter user objects based on param
+        let allUsers = this.get('store').peekAll('user');
+        var filteredUsers = allUsers.filter((item) => item.get('fullName').match(regex));
+
+        //Get each user's group memberships, merge them into one RSVP promise
+        var filteredUsersMemberships = filteredUsers.getEach('memberships');
+        var allMemberships =  Ember.RSVP.all(filteredUsersMemberships).then(function(membershipCollections){
+          return membershipCollections.reduce(function(sum, val){
+            return sum.pushObjects(val.toArray());
           }, []);
-          return memberships;
         });
+
+        //TODO: ensure results only contains current user's memberships
+        return allMemberships;
+
       } else {
-        return this.get('sessionUser.user.memberships')
+
+        return this.get('sessionUser.user.memberships');
+
       }
     }
   }
