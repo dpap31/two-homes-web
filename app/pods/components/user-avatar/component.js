09d43f671d9ff3721dc719d9f7a1d179c5973bb1
Drew Pappas <drewpappas@mac.com>
2016-06-12 16:14:15 -0700

    added avatar component

--- app/pods/components/user-avatar/component.js
+++ app/pods/components/user-avatar/component.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  sessionUser: Ember.inject.service('session-user'),
+
   avatar_bg_color: Ember.computed(function(){
+    var user = this.get('user');
+    // Use fullName and firstName length to generate same bg colors
+    var fullNameLength = user.get('fullName.length');
+    var firstNameLength = user.get('firstName.length');
     const colors = ['#000000', '#f06d06', '#335CD6', '#6B4724', '#0000DB', '#6E6E6E', '#001947'];
     const arrLength = colors.length;
-    var colorOne = colors[Math.floor(Math.random()*100) % arrLength];
-    var colorTwo = colors[Math.floor(Math.random()*100) % (arrLength)];
+    var colorOne = colors[ fullNameLength % arrLength];
+    var colorTwo = colors[ (firstNameLength * 3) % arrLength];
+    // Output style text in html safe string
     var str = "background: linear-gradient(" + colorOne + ", " + colorTwo + ")"
     return Ember.String.htmlSafe(str);
   }),
