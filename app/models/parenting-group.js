1fee65447722b07bbd341582a8be5003ce215bd0
Drew Pappas <drewpappas@mac.com>
2016-06-28 18:09:10 -0700

    added in place editing of parenting group name

--- app/models/parenting-group.js
+++ app/models/parenting-group.js
@@ -1,10 +1,14 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
+  name: DS.attr('string'),
   memberships: DS.hasMany('memberships'),
   users: DS.hasMany('users'),
   invites: DS.hasMany('invites'),
-  conversations: DS.hasMany('conversations')
+  conversations: DS.hasMany('conversations'),
+
+  isNotValid: Ember.computed.empty('name'),
 });
--- app/pods/app/parenting-group/route.js
+++ app/pods/app/parenting-group/route.js
@@ -9,4 +9,25 @@ export default Ember.Route.extend({
     let route = this;
     return route.store.findRecord('parentingGroup', params.parenting_group_id);
   },
+
+  actions: {
+
+     editPgName(pg) {
+       pg.set('isEditing', true);
+     },
+
+     cancelPgEdit(pg) {
+       pg.set('isEditing', false);
+       pg.rollbackAttributes();
+     },
+
+     savePg(pg) {
+       if (pg.get('isNotValid')) {
+         return;
+       }
+
+       pg.set('isEditing', false);
+       pg.save();
+     }
+   }
 });
