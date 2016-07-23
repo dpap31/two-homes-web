aa4bcd412e891f644364f3c98566f3a7a049e8b7
Drew Pappas <drewpappas@mac.com>
2016-07-23 14:56:06 -0700

    added new invite handler

--- app/pods/admin/group/users/controller.js
+++ app/pods/admin/group/users/controller.js
@@ -1,10 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    removeUserFromGroup(user, group) {
-      group.get('users').removeObject(user);
-      group.save();
+    emailAddress: '',
+    isAddingUser: false,
+    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: Ember.computed.not('isValid'),
+
+    actions:{
+      sendInvite(){
+        let controller = this;
+        let invite = controller.get('model');
+
+        let onSuccess = function(){
+          controller.get('flashMessages').success(`Invitation sent to: ${controller.get('emailAddress')}`);
+          controller.set('emailAddress', '');
+          controller.toggleProperty('isAddingUser');
+        };
+
+        let onFailure = function(){
+          controller.get('flashMessages').danger('Error sending invitation. Try again later.');
+        };
+
+        invite.set('email', controller.get('emailAddress'));
+        invite.save().then(onSuccess, onFailure);
+      },
+
+      removeUserFromGroup(user, group) {
+        group.get('users').removeObject(user);
+        group.save();
+      },
+
+      addUserToggle(){
+        this.toggleProperty('isAddingUser');
+      }
     }
-  }
-});
+  });
+
